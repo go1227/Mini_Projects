fd93180cbf38b9d9156cdd91332e351f74c78380
go1227 <go1227@gmail.com>
2021-06-27 13:42:59 -0400

    Definition of starting search folder as an argument

--- Tools/list_all_files_and_folders.py
+++ Tools/list_all_files_and_folders.py
@@ -2,16 +2,24 @@ __author__ = "Gil Ortiz"
 __version__ = "1.0"
 __date_last_modification__ = "2/29/2020"
 __python_version__ = "3"
-__notes__ = "PEP8 compliant"
+__notes__ = "PEP8 compliant | command line execution"
 
 import os
+import sys
 
-start = '/'
 
-try:
-	with os.scandir(start) as entries:
-		for entry in entries:
-			print(entry.name)
-except Exception as e:
-	print(f'Failed to read the folder {start}: '+ str(e))
-	# raise
+def main():
+    start = '/'
+    if len(sys.argv) > 1:
+        start = sys.argv[1]
+    try:
+        with os.scandir(start) as entries:
+            for entry in entries:
+                print(entry.name)
+    except Exception as e:
+        print(f'Failed to read the folder {start}: ' + str(e))
+    # raise
+
+
+if __name__ == '__main__':
+    main()
